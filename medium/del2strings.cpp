652920c22afba21849890e4a88e795981b17394b
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-06-14 16:13:44 +0530

    :tada:

--- medium/del2strings.cpp
+++ medium/del2strings.cpp
@@ -4,13 +4,13 @@ using namespace std;
 class Solution {
 public:
     int lcs (string s1, string s2, int m, int n, vector<vector<int>> &dp){
-        if (m== || n==0) return 0;
+        if (m==s1.length() || n==s2.length()) return 0;
         if (dp[m][n]!=-1) return dp[m][n];
-        if (s1[m-1] == s2[n-1]) return dp[m][n] = 1 + lcs(s1, s2, m-1, n-1, dp);
-        else return dp[m][n] = max(lcs(s1, s2, m, n-1, dp), lcs(s1, s2, m-1, n, dp));
+        if (s1[m] == s2[n]) return dp[m][n] = 1 + lcs(s1, s2, m+1, n+1, dp);
+        else return dp[m][n] = max(lcs(s1, s2, m, n+1, dp), lcs(s1, s2, m+1, n, dp));
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1, -1));
-        return lcs(word1, word2, 0, 0, dp);
+        vector<vector<int>> dp(word1.size(), vector<int>(word2.size(), -1));
+        return word1.length()+word2.length()-2*lcs(word1, word2, 0, 0, dp);
     }
 };
