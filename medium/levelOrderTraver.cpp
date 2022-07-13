646f1bf5dc3621b9526b59aa5d4520dc02769d59
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-07-13 09:39:40 +0530

    DAily

---
+++ medium/levelOrderTraver.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<vector<int>> res;
+
+        if (root!=nullptr) q.push(root);
+
+        while (!q.empty()){
+            vector<int> level;
+            int sz = q.size();
+
+            while (sz--){
+                level.push_back(q.front()->val);
+                if (q.front()->left) q.push(q.front()->left);
+                if (q.front()->right) q.push(q.front()->right);
+                q.pop();
+            }
+            res.push_back(level);
+        }
+        return res;
+        
+    }
+};
