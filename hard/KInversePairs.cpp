ebea0ca61ce6ef06255e6f9398adf7a09015bf47
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-07-17 22:39:33 +0530

    tabular dp still TLE

--- hard/KInversePairs.cpp
+++ hard/KInversePairs.cpp
@@ -2,30 +2,21 @@
 using namespace std;
 
 class Solution {
-public:
-    vector<vector<int>> dp;
-    int m, p, M=1000000007;
-
-    int  addEle(int n, int k){
-        if (k==p) return 1; //condition in this permutation satisfied
-        if (n==m+1) return 0; //out of bound
-        if (dp[n][k] != -1) return dp[n][k];
-
-        int res=0;
-        // number of ways to place (n)th num in array
-        for (int i=n; i>=max(1, n-(p-k)); i--){ 
-            // place at (i)th pos in the array
-            int v = addEle(n+1, k+(n-i)) % M;
-            cout << n << " " << k << " " << i << " " << v << endl;
-            res = (res+v) % M;
-        }
-        
-        return dp[n][k] = res;
-    }
+    public:
     int kInversePairs(int n, int k) {
-        m=n; p=k;
-        dp.resize(n+1, vector<int>(k+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(k+1, 0));
 
-        return addEle(1, 0);
+        for (int i = 1; i <= n; i++) { //placing i th number
+            for (int j = 0; j <= k; j++) { // saving states for pairs to be made
+                if (j == 0) dp[i][j] = 1;
+                else {
+                    for (int p = 0; p <= min(j, i - 1); p++) 
+                        // i can generate p pairs from this number i
+                        // find the permutations for j-p pairs
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - p]) % 1000000007;
+                }
+            }
+        }
+        return dp[n][k];
     }
 };
