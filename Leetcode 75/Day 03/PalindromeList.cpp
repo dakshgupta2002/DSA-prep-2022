f1ddbf1944c3fd2b5b2662ff5a5d4dc47f62f08f
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-07-24 23:10:45 +0530

    n space sol

---
+++ Leetcode 75/Day 03/PalindromeList.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    bool isPal(string &s){
+        int i=0, j=s.length()-1;
+        while (i<j){
+            if (s[i]!=s[j]) return false;
+            i++; j--;
+        }
+        return true;
+    }
+    bool isPalindrome(ListNode* head) {
+        string s="";
+        while (head){
+            s+=(head->val + '0');
+            head=head->next;
+        }      
+        return isPal(s);
+    }
+};
