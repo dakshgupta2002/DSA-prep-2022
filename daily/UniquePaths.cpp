af649b4c3567c9f067af96fe00244786616b3373
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-08-01 22:54:17 +0530

    bottom up, linear space

--- daily/UniquePaths.cpp
+++ daily/UniquePaths.cpp
@@ -3,19 +3,14 @@ using namespace std;
 
 class Solution {
 public:
-    int p, q;
-    vector<vector<int>> dp;
-    int dfs(int i, int j){
-        if (i==p || j==q) return 0;
-        if (i==p-1 && j==q-1) return 1;
-        if (dp[i][j] != -1) return dp[i][j];
-
-        return dp[i][j] = dfs(i+1, j) + dfs(i, j+1);
-    }
     int uniquePaths(int m, int n) {
-        p=m; q=n;
-        dp.resize(m, vector<int>(n, -1));
+        vector<int> dp(n, 1);
 
-        return dfs(0, 0);
+        for (int i=m-2; i>=0; i--){
+            for (int j=n-2; j>=0; j--){
+                dp[j] = dp[j+1];
+            }
+        }
+        return dp[0];
     }
 };
