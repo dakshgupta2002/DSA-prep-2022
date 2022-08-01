47362e4921696cd9b07d2cec053bbe33f2896849
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-08-01 22:56:39 +0530

    commented

--- daily/UniquePaths.cpp
+++ daily/UniquePaths.cpp
@@ -4,11 +4,14 @@ using namespace std;
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> dp(n, 1);
+        vector<int> dp(n, 1); 
+        //we only need to maintain the data of current row
 
         for (int i=m-2; i>=0; i--){
             for (int j=n-2; j>=0; j--){
-                dp[j] = dp[j+1];
+                // dp[j] is the value from the bottom 
+                // and dp[j+1] is from left
+                dp[j] += dp[j+1];
             }
         }
         return dp[0];
