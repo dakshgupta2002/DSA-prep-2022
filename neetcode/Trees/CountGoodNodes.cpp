945810db8c76d97d9c3a126825d7caff70eb35e7
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-08-24 23:15:41 +0530

    Good nodes in tree

---
+++ neetcode/Trees/CountGoodNodes.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int ans = 0;
+    void findGood(TreeNode* root, int max){
+        if (root==NULL) return;
+        if (root->val > max){
+            ans++;
+            findGood(root->left, root->val);
+            findGood(root->right, root->val);
+        }else{
+            findGood(root->left, max);
+            findGood(root->right, max);
+        }
+    }
+
+    int goodNodes(TreeNode* root) {
+        findGood(root, INT32_MIN);
+        return ans;
+    }
+};
